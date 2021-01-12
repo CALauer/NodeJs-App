d277fba43542f56ee213db3116ffb8f2ed901079
Chris Lauer <56165582+CALauer@users.noreply.github.com>
2021-01-12 08:01:34 -0500

    Update database.js

--- database.js
+++ database.js
@@ -4,7 +4,7 @@ const db_password = process.env.DB_PASS
 const db_name = process.env.DB_NAME 
 
 var mysql      = require('mysql');
-var conn = mysql.createConnection({
+var connection = mysql.createConnection({
   host     : db_host,
   user     : db_user,
   password : db_password,
@@ -18,4 +18,4 @@ if(!err) {
     console.log("Error while connecting with database");
 }
 });
-module.exports = conn;
+module.exports = connection;
