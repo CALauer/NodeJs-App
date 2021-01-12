a08faa04ddc9d13ff613685a4391564101f287e7
Chris Lauer <56165582+CALauer@users.noreply.github.com>
2021-01-12 10:02:47 -0500

    Update Test

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 require('dotenv').config()
+const db_host = process.env.DB_HOST
+const db_user = process.env.DB_USER
+const db_password = process.env.DB_PASS
+const db_name = process.env.DB_NAME 
 const morgan = require('morgan');
 const express = require('express')
-const session = require('express-session')
+var session = require('express-session');
+var MySQLStore = require('express-mysql-session')(session);
 const router = express.Router()
 const bodyParser=require('body-parser');
 const app = express()
@@ -11,14 +16,29 @@ const loginRouter = require('./routes/login');
 const registrationRouter = require('./routes/registration');
 const dashboardRouter = require('./routes/dashboard');
 const logoutRouter = require('./routes/logout');
-const globalRouter = require('./routes/global.js')
+const globalRouter = require('./routes/global.js');
+var sessionStore = new MySQLStore(session);
+let mysql = require('mysql2/promise')
 app.set('trust proxy', 1);
 app.set('view engine','ejs');
 app.use(express.static('views'))
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(session({ 
+
+
+var options = {
+  connectionLimit : 10,
+  host     : db_host,
+  user     : db_user,
+  password : db_password,
+  database : db_name
+}
+
+var connection = mysql.createPool(options); // or mysql.createPool(options);
+var sessionStore = new MySQLStore({}, connection);
+
+app.use(session({
+    store: sessionStore,
     secure: true,
     secret: '123456cat',
     resave: false,
@@ -26,12 +46,7 @@ app.use(session({
     cookie: { maxAge: 60000 },
     resave: false
   }))
-  app.use(function(req,res,next){
-    if(!req.session){
-        return next(new Error('Oh no')) //handle error
-    }
-    next() //otherwise continue
-    });
+
 
 // Routes
 app.use('/', registrationRouter);
@@ -39,6 +54,8 @@ app.use('/', dashboardRouter);
 app.use('/', loginRouter);
 app.use('/', logoutRouter);
 app.use('/', globalRouter);
+
+
 
 
 
