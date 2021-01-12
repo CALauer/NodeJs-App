ead78d3fb7fd2f634d3187a677419748d6ca3000
Chris Lauer <56165582+CALauer@users.noreply.github.com>
2021-01-12 15:00:39 -0500

    Update login.js

--- routes/login.js
+++ routes/login.js
@@ -11,43 +11,35 @@ router.post('/login', function(req, res){
     let email = req.body.email_address;
     let password = req.body.password;
     let sql='SELECT * FROM users WHERE email =?';
-    db.query(sql, [email, password], function (err, data, fields) {
-        let randomString = bcrypt.hashSync('/,;tT5%6', 8);
-            if(data.length > 0){
-                let dbpass = data[0].password
-                if(bcrypt.compareSync(password,dbpass)){
-                    req.session.loggedinUser= true;
-                    req.session.email = email;
-                    console.log("Logged in")
-                    db.releaseConnection(db);
-                    res.redirect('/dashboard'), {};
-                } else if (bcrypt.compareSync(randomString, dbpass)){
-                    res.render('login-form',{alertMsg:"Password Incorrect"});
-                    console.log("2nd else")
-                    db.releaseConnection(db);
-                } else {
-                    res.render('login-form',{alertMsg:"Email Incorrect"});
-                    console.log("3rd else")
-                    db.releaseConnection(db);
-                }         
+
+    db.query('SELECT * FROM users WHERE email = ?',[email], async function (error, results, fields) {
+        if (error) {
+          res.send({
+            "code":400,
+            "failed":"error ocurred"
+          })
+        }else{
+          if(results.length >0){
+            const comparision = await bcrypt.compare(password, results[0].password)
+            if(comparision){
+                req.session.loggedinUser= true;
+                req.session.email = email;
+                res.redirect('/dashboard')
+            }
+            else{
+              res.send({
+                   "code":204,
+                   "success":"Email and password does not match"
+              })
+            }
+          }
+          else{
+            res.send({
+              "code":206,
+              "success":"Email does not exits"
+                });
+          }
         }
-    })  
-})
-//             if(data[0].length > 0){
-//             let dbpass = data[0].password
-//             if(bcrypt.compareSync(password,dbpass)){
-//                 req.session.loggedinUser= true;
-//                 req.session.email = email;
-//                 console.log(email)
-//                 res.redirect('dashboard');
-//             }
-//             else {
-//                 res.render('login-form',{alertMsg:"Password Incorrect"});
-                
-//             }
-//         }else{
-//             res.render('login-form',{alertMsg:"Email Incorrect"});
-//         }
-//     })
-// })
+        });
+    })
 module.exports = router;
