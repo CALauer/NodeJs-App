32d58ea51173c4fcf87c83f3eddc62de700146a3
Chris Lauer <56165582+CALauer@users.noreply.github.com>
2021-01-12 11:42:57 -0500

    Update Test

--- app.js
+++ app.js
@@ -2,15 +2,12 @@ require('dotenv').config()
 
 const morgan = require('morgan');
 const express = require('express')
-const app = express()
-const mysql = require('mysql2/promise')
-
-var session = require('express-session');
-var MySQLStore = require('express-mysql-session')(session);
+const session = require('express-session');
 const router = express.Router()
 const bodyParser=require('body-parser');
-var PORT = process.env.PORT || 5000;
+let PORT = process.env.PORT || 5000;
 
+const app = express()
 
 
 // My Defined Routes
@@ -20,7 +17,6 @@ const dashboardRouter = require('./routes/dashboard');
 const logoutRouter = require('./routes/logout');
 const globalRouter = require('./routes/global.js');
 
-var sessionStore = new MySQLStore(session);
 app.set('trust proxy', 1);
 app.set('view engine','ejs');
 app.use(express.static('views'))
@@ -28,47 +24,6 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
 
-var options = {
-  connectionLimit : 1,
-  host     : process.env.DB_HOST,
-  user     : process.env.DB_USER,
-  password : process.env.DB_PASS,
-  database : process.env.DB_NAME,
-  clearExpired: true,
-  createDatabaseTable: true,
-  charset: 'utf8mb4_bin',
-	schema: {
-		tableName: 'sessions',
-		columnNames: {
-			session_id: 'session_id',
-			expires: 'expires',
-			data: 'data'
-		}
-	}
-}
-
-let sessConnect = mysql.createPool(options); // or mysql.createPool(options);
-var sessionStore = new MySQLStore({}, sessConnect);
-
-app.set('trust proxy', 1) 
-// trust first proxy
-app.use(session({
-  key: 'session_cookie_name',
-  secret: 'session_cookie_secret',
-  store: sessionStore,
-  resave: false,
-  saveUninitialized: false
-}))
-
-process.on('uncaughtException', function (err) {
-  console.log(err);
-}); 
-  app.use(function(req,res,next){
-    if(!req.session){
-        return next(new Error('Oh no')) //handle error
-    }
-    next() //otherwise continue
-    });
 
 
 // Routes
--- database.js
+++ database.js
@@ -1,3 +1,5 @@
+
+
 var mysql = require('mysql2');
 
 var connection = mysql.createPool({
@@ -5,7 +7,10 @@ var connection = mysql.createPool({
   host     : process.env.DB_HOST,
   user     : process.env.DB_USER,
   password : process.env.DB_PASS,
-  database : process.env.DB_NAME 
+  database : process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0 
 });
 
 // pool.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
