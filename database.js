7087f48b7ecf0efd698188d31ff1ac0d3921e834
Chris Lauer <56165582+CALauer@users.noreply.github.com>
2021-01-12 16:17:59 -0500

    Update database.js

--- database.js
+++ database.js
@@ -2,19 +2,10 @@
 var mysql = require('mysql2');
 
 var connection = mysql.createPool({
-  connectionLimit : 10,
-  host     : process.env.DB_HOST,
-  user     : process.env.DB_USER,
-  password : process.env.DB_PASS,
-  database : process.env.DB_NAME,
-  waitForConnections: true
-});
-
-connection.query(
-  'SELECT * FROM `users`',
-  function(err, results) {
-    console.log(results);
-  }
-);
+    host     : '162.241.226.190', // MYSQL HOST NAME
+    user     : 'chrislc9_admin', // MYSQL USERNAME
+    password : ',6Lc3.T)oD]*', // MYSQL PASSWORD
+    database : 'chrislc9_db1' // MYSQL DB NAME
+}).promise();
 
 module.exports = connection;
