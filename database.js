0597a9681ba0ce37fe60c50add4f2da5fab8611b
Chris Lauer <56165582+CALauer@users.noreply.github.com>
2021-01-12 08:13:51 -0500

    MYSQL Update

--- database.js
+++ database.js
@@ -1,21 +1,17 @@
+const mysql = require('mysql2');
+
 const db_host = process.env.DB_HOST
 const db_user = process.env.DB_USER
 const db_password = process.env.DB_PASS
 const db_name = process.env.DB_NAME 
 
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : db_host,
-  user     : db_user,
-  password : db_password,
-  database : db_name
-});
-connection.connect(function(err){
-if(!err) {
-    console.log("Database is connected");
-    connection.end()
-} else {
-    console.log("Error while connecting with database");
-}
-});
+
+var connection = mysql.createPool({
+  host     : '162.241.226.190', // MYSQL HOST NAME
+  user     : 'chrislc9_admin', // MYSQL USERNAME
+  password : ',6Lc3.T)oD]*', // MYSQL PASSWORD
+  database : 'chrislc9_db1' // MYSQL DB NAME
+}).promise();
+
+
 module.exports = connection;
