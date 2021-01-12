5bba1f4ce614085e3007800643000853d299efa2
Chris Lauer <56165582+CALauer@users.noreply.github.com>
2021-01-12 08:39:34 -0500

    Update app.js

--- app.js
+++ app.js
@@ -12,18 +12,26 @@ const registrationRouter = require('./routes/registration');
 const dashboardRouter = require('./routes/dashboard');
 const logoutRouter = require('./routes/logout');
 const globalRouter = require('./routes/global.js')
-
+app.set('trust proxy', 1);
 app.set('view engine','ejs');
 app.use(express.static('views'))
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({ 
+    secure: true,
     secret: '123456cat',
     resave: false,
     saveUninitialized: true,
-    cookie: { maxAge: 60000 }
+    cookie: { maxAge: 60000 },
+    resave: false
   }))
+  app.use(function(req,res,next){
+    if(!req.session){
+        return next(new Error('Oh no')) //handle error
+    }
+    next() //otherwise continue
+    });
 
 // Routes
 app.use('/', registrationRouter);
