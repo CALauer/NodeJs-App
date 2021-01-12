0e8653c5ff8d71125c5a3422b1a869de01483499
Chris Lauer <56165582+CALauer@users.noreply.github.com>
2021-01-12 08:29:09 -0500

    MYSQL UPDATE

--- database.js
+++ database.js
@@ -1,17 +1,21 @@
-const mysql = require('mysql2');
-
 const db_host = process.env.DB_HOST
 const db_user = process.env.DB_USER
 const db_password = process.env.DB_PASS
 const db_name = process.env.DB_NAME 
 
+var mysql = require('mysql');
 
 var connection = mysql.createPool({
-  host     : '162.241.226.190', // MYSQL HOST NAME
-  user     : 'chrislc9_admin', // MYSQL USERNAME
-  password : ',6Lc3.T)oD]*', // MYSQL PASSWORD
-  database : 'chrislc9_db1' // MYSQL DB NAME
-}).promise();
+  connectionLimit : 10,
+  host     : db_host,
+  user     : db_user,
+  password : db_password,
+  database : db_name
+});
 
+// pool.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+//   if (error) throw error;
+//   console.log('The solution is: ', results[0].solution);
+// });
 
 module.exports = connection;
--- routes/login.js
+++ routes/login.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var db=require('../database');
 var bcrypt = require('bcryptjs');
+
 /* GET users listing. */
 router.get('/login', function(req, res, next) {
   res.render('login-form');
