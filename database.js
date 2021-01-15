6e331c7172483c324048fbc3af3251e7c8fe2a90
Chris Lauer <56165582+CALauer@users.noreply.github.com>
2021-01-15 05:19:52 -0500

    Update database.js

--- database.js
+++ database.js
@@ -2,11 +2,12 @@
 var mysql = require('mysql2');
 
 var connection = mysql.createPool({
-    host     : '162.241.226.190', // MYSQL HOST NAME
-    user     : 'chrislc9_admin', // MYSQL USERNAME
-    password : ',6Lc3.T)oD]*', // MYSQL PASSWORD
-    database : 'chrislc9_db1' // MYSQL DB NAME
+    host     :  process.env.DB_HOST, // MYSQL HOST NAME
+    user     : process.env.DB_USER, // MYSQL USERNAME
+    password : process.env.DB_PASS, // MYSQL PASSWORD
+    database : process.env.DB_NAME // MYSQL DB NAME
 })
+
 
 
 module.exports = connection;
